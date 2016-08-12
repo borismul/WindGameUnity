7e1b4249aa71e83eacdded846ba6822f2fc17aaa
Stephen <stephenverbist@gmail.com>
2016-08-12 15:45:19 +0200

    Changed the way turbines are built. The buildObject method of terraincontroller is no longer called because terraincontroller should not create turbines. However, you are now again able to build turbines on top of eachother. This will need fixing.
    Added a game resources class that takes care of the wealth and production a player has and removed the tracking of these functions from the world controller
    
    Also added a turbine manager that keeps track of the turbines in the world. Removed this function from the world controller.

--- WindGame/Assets/Scripts/Mission1Controller.cs
+++ WindGame/Assets/Scripts/Mission1Controller.cs
@@ -13,7 +13,7 @@ public class Mission1Controller : MonoBehaviour {
     [SerializeField]
     GameObject UIObj;
 
-    WorldController world;
+    // WorldController world;
     UIScript ui;
 
 
@@ -21,7 +21,7 @@ public class Mission1Controller : MonoBehaviour {
 	void Start () {
         objectivesCompleted = new bool[] { false, false, true };
         objectives = new string[] { "Generate 200 power before 830 AD", "Get 1000 capital before 850 AD", "Keep public acceptance above -40" };
-        world = worldObj.GetComponent<WorldController>();
+        // world = worldObj.GetComponent<WorldController>();
         ui = UIObj.GetComponent<UIScript>();
 
         obj1Date = new System.DateTime(830, 1, 1);
@@ -30,33 +30,33 @@ public class Mission1Controller : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        if (world.totalPower >= 200)
-        {
-            objectivesCompleted[0] = true;
-        } else
-        {
-            objectivesCompleted[0] = false;
-        }
+        // if (world.totalPower >= 200)
+        // {
+        //     objectivesCompleted[0] = true;
+        // } else
+        // {
+        //     objectivesCompleted[0] = false;
+        // }
 
-        if (world.capital >= 1000)
-        { 
-            objectivesCompleted[1] = true;
-        } else
-        {
-            objectivesCompleted[1] = false;
-        }
+        // if (world.capital >= 1000)
+        // { 
+        //     objectivesCompleted[1] = true;
+        // } else
+        // {
+        //     objectivesCompleted[1] = false;
+        // }
 
-        if (System.DateTime.Compare(world.date, obj1Date) > 0 && !objectivesCompleted[0])
-            gameOver();
+        // if (System.DateTime.Compare(world.date, obj1Date) > 0 && !objectivesCompleted[0])
+        //     gameOver();
 
-        if (System.DateTime.Compare(world.date, obj2Date) > 0 && !objectivesCompleted[1])
-            gameOver();
+        // if (System.DateTime.Compare(world.date, obj2Date) > 0 && !objectivesCompleted[1])
+        //     gameOver();
 
-        if (world.publicAcceptance < -40)
-            gameOver();
+        // if (world.publicAcceptance < -40)
+        //     gameOver();
 
-        if (objectivesCompleted[0] && objectivesCompleted[1])
-            gameWon();
+        // if (objectivesCompleted[0] && objectivesCompleted[1])
+        //     gameWon();
 
 	}
 
