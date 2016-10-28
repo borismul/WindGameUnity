3b18378b3ed12743985cb569c5816a2fdea276f3
Boris Mulder <boris.mulder@live.nl>
2016-10-28 02:21:01 +0200

    Fixed a small error that the toggle in the tile information menu would stay turned on when the tile information was closed.

--- WindGame/Assets/Scripts/TileInfomationMenu.cs
+++ WindGame/Assets/Scripts/TileInfomationMenu.cs
@@ -33,6 +33,8 @@ public class TileInfomationMenu : MonoBehaviour {
         terrain = TerrainController.thisTerrainController;
         if (terrain != null)
             WindVisualizer.instance.StopVisualizeWind();
+
+        toggle.isOn = false;
     }
 	
     void ChangeWindHeight(float value)
