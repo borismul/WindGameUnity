11f7dd70d9007a82d92005fde28b0b846ab49b07
Jonathan Hoogvliet <j.m.hoogvliet@student.tudelft.nl>
2017-03-30 14:48:08 +0200

    Main menu turbine addition

---
+++ WindGame/Assets/MainMenuTurbine.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuTurbine : MonoBehaviour {
+
+    public Image rotor;
+    public float rotationSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        rotor.transform.localRotation = Quaternion.Euler(rotor.transform.localRotation.eulerAngles + new Vector3(0, 0, rotationSpeed)*Time.deltaTime);
+	}
+}
