ad94674cd01fc02a317ea1b7e829985814096910
Boris Mulder <boris.mulder@live.nl>
2017-03-30 11:13:17 +0200

    - Update to Unity 5.2.2f1 Added a plot function (not finished yet)

---
+++ WindGame/Assets/TestPlot.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class TestPlot : MonoBehaviour {
+
+    public GameObject linePrefab;
+    void Start()
+    {
+
+        GameObject testPanel = gameObject;
+
+        List<float> xData = new List<float>();
+        List<float> yData = new List<float>();
+
+        for (float x = -10; x < 10; x += 0.1f)
+        {
+            xData.Add(x);
+            yData.Add(x * x);
+        }
+
+        Plot plot = new Plot(testPanel, xData, yData, Color.green, .5f, linePrefab);
+    }
+}
