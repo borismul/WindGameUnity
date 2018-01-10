c68292fd0403bc63f311c833ea0fc9c6a10e3f06
Jonathan Hoogvliet <j.m.hoogvliet@gmx.com>
2018-01-11 00:21:53 +0100

    Several functionalities of the Persian mission have been upgraded - Nine different house models are spawned during the expansion of the city - The UI canvas of the Persian mission functions following the non-scriptable menu system - Old Persian build menu is inserted in the system, but is disfunctional
    Note for development: The 'MenuManager' script is updated to accomodate the UI system in the mission. Therefore this script does not work in the main menu anymore. A workaround is to either check the scene in this script, or create separate managers for main menu and mission specific goals.

---
+++ WindGame/Assets/UIMenu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class UIMenu : SimpleMenu<UIMenu> {
+
+	public void OnMenuPressed()
+    {
+        Debug.Log("Menu button is pressed");
+    }
+
+    public void OnBuildPressed()
+    {
+        Debug.Log("Build button is pressed");
+        BuildMenu.Show();
+    }
+
+    public void OnFFPressed()
+    {
+        Debug.Log("Fast Forward (FF) is pressed");
+    }
+
+    public void OnPausePressed()
+    {
+        Debug.Log("Pause button is pressed");
+    }
+
+    public void OnInfoPressed()
+    {
+        Debug.Log("Info button is pressed");
+    }
+
+    public void OnHelpPressed()
+    {
+        Debug.Log("Help button is pressed");
+    }
+}
