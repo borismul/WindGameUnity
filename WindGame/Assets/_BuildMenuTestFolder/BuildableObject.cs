0e1a90341ba63b1cdd384c0ac8e9e0412c32e0da
Jonathan Hoogvliet <j.m.hoogvliet@gmx.com>
2018-01-16 13:36:42 +0100

    Added a BuildableObject script from which objects that can be built inherit

---
+++ WindGame/Assets/_BuildMenuTestFolder/BuildableObject.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+// This class is meant to define the base properties of a buildable object.
+
+public class BuildableObject : MonoBehaviour {
+
+#if UNITY_EDITOR
+    [Multiline]
+    public string developerDescription = "Description of what this is";
+#endif
+
+    public string longName = "Buildable Object";    // Name for use in GUI elements
+    public string shortName = "Buildable";          // Name for use on buttons (smaller)
+
+    [Multiline] // Background information for use in build menu
+    public string backgroundInfo = "Background information that characterises this buildable element";   
+
+    public bool canRotateOnBuild = true;
+    public bool canBeBuiltInSea = false;
+
+    public float buildingCost;      // Cost of building the object
+    [HideInInspector]
+    public Vector3 worldPosition;   // Position in the world
+    [HideInInspector]
+    public float timeBuilt;         // Time in-game at which the object is built
+
+}
---
+++ WindGame/Assets/_BuildMenuTestFolder/WeatherstationObject.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class WeatherstationObject : BuildableObject {
+
+    [Header("Weatherstation model parts")]
+    public GameObject weatherStation;
+
+    private float windDirection;
+    private float windMagnitude;
+
+    private void MeasureWind()
+    {
+        windDirection += 0.1f;
+        windMagnitude = 1;
+    }
+
+    private void Update()
+    {
+        MeasureWind();
+    }
+
+}
